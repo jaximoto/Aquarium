d490ccc7cdf32f5232bbb96c29c95db116b7bc11
Hooninator <jbellavita@berkeley.edu>
2024-11-23 15:18:33 -0800

    FUCK YOU

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -1,9 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-abstract class Item
+public abstract class Item
 {
     public Dictionary<string, int> resourceCosts = new Dictionary<string, int>();
-
-
 }
--- Assets/Scripts/PlayerStatsModel.cs
+++ Assets/Scripts/PlayerStatsModel.cs
@@ -24,9 +24,11 @@ public class PlayerStatsModel {
 
     public void Buy(Item i)
     {
-        foreach(string s in i.resources)
+        /*
+        foreach(string s in i.resourceCosts)
         {
-            ChangeStat(s, -i.resources[s]);
+            ChangeStat(s, -i.resourceCosts[s]);
         }
+        */
     }
 }
