c3b70d5487ddcc14a5d38d8d929e1c884461031a
yibbyyy <106987734+yibbyyy@users.noreply.github.com>
2024-11-24 15:00:38 -0800

    uicontrooll

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    public GameObject shopTab;
+    public GameObject fishTab;
+    public GameObject tankTab;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
