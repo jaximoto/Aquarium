738810261f1a8c6da8d4321563f5e3448c6cd594
Hooninator <jbellavita@berkeley.edu>
2024-11-24 14:36:07 -0800

    jew

--- Assets/Scripts/Player/PlayerStatsController.cs
+++ Assets/Scripts/Player/PlayerStatsController.cs
@@ -1,12 +1,15 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class PlayerStatsController : MonoBehaviour
 {
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public PlayerStatsModel model;
+
     void Awake()
     {
-        PlayerStatsModel model = new PlayerStatsModel();
+        this.model = new PlayerStatsModel();
         //ShopController.OnBuyPlayer += model.Buy();
         ShopController.OnBuyPlayer += model.Buy;
         // TODO subscribe the model buy method to the shop OnBuy event
@@ -17,4 +20,15 @@ public class PlayerStatsController : MonoBehaviour
     {
         
     }
+
+    public bool CanBuy(Dictionary<string, int> costs)
+    {
+        foreach(KeyValuePair<string, int> pair in costs)
+        {
+            if (this.model.model[pair.Key] < pair.Value)
+                return false;
+        }
+        return true;
+    }
+
 }
