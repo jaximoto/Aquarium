7811aba38aa5dda09f15a2bce6b99981860129ec
Hooninator <jbellavita@berkeley.edu>
2024-11-25 04:30:56 -0800

    pen

--- Assets/Scripts/ShopItems/Consumable.cs
+++ Assets/Scripts/ShopItems/Consumable.cs
@@ -5,5 +5,11 @@ public class Consumable : Item
 {
     public float fishStatMod, fishHungerMod, tempMod, PHMod, CO2Mod, algaeMod, wasteMod;
 
+    public override void UpdateSelf(ref TankModel tankModel)
+    {
+
+        tankModel.myItems.Remove(this);
+        Destroy(this.gameObject);
+    }
 
 }
