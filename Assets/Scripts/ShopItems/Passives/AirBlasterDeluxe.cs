8beb088c3f5f895648e024e98e3e4c9beaef2645
Hooninator <jbellavita@berkeley.edu>
2024-11-25 03:18:34 -0800

    shit pants

--- Assets/Scripts/ShopItems/Passives/AirBlasterDeluxe.cs
+++ Assets/Scripts/ShopItems/Passives/AirBlasterDeluxe.cs
@@ -3,12 +3,6 @@ using UnityEngine;
 
 public class AirBlasterDeluxe : Passive
 {
-    public override void UpdateSelf()
-    {
-        return;
-    }
-
-
     public override void UpdateTank(ref TankModel tankModel)
     {
         tankModel.IncrementStat("CO2", this.CO2Mod);
--- Assets/Scripts/ShopItems/Passives/Heater1.cs
+++ Assets/Scripts/ShopItems/Passives/Heater1.cs
@@ -4,12 +4,6 @@ using UnityEngine;
 public class Heater1 : Passive
 {
 
-    public override void UpdateSelf()
-    {
-        return;
-    }
-
-
     public override void UpdateTank(ref TankModel tankModel)
     {
         tankModel.IncrementStat("Temp", this.tempMod);
